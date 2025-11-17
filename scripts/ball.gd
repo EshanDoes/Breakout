f5c9e1942fbc69a49e8c87b73348c14612ddffbb
EshanDoes <eshan.shiraz@gmail.com>
2025-11-17 17:37:58 -0500

    Hourly Commit 5.5? (Day 8)
    Added a simple visualization for lives, removed the limit for the paddle since it felt a lot better that way, and made a slight optimization with the ball

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -5,18 +5,19 @@ var initialVelocity # Initial velocity (speed and direction)
 var launched = 0
 var bricksDestroyed = 0
 var bricksGroup
+var lives = 3
 
 # Set the initial velocity because the speed variable doesn't apply until the project starts
 func _ready():
 	initialVelocity = Vector2(speed, -200)
-	bricksGroup = $"root/Main/Bricks".new()
+	bricksGroup = $"/root/Main/Bricks"
 
 # Check if the player clicked, and launch the ball if they did, and if they didn't make the ball follow the paddle when the mouse moves
 func _input(event):
 	if event.is_action_pressed("click"):
 		launched = 1
 	if launched == 0:
-		self.position.x = $"/root/Main/Paddle".position.x - 320
+		self.position.x = $"/root/Main/Paddle".position.x
 
 # Makes the ball move and bounce
 func _physics_process(delta):
@@ -36,11 +37,22 @@ func _physics_process(delta):
 			collider.get_collider().queue_free()
 			bricksDestroyed += 1
 			if bricksDestroyed == 20:
-				$"rootMain".add_child(bricksGroup)
 				print("All bricks destroyed!")
+				add_sibling(bricksGroup)
 			print(bricksDestroyed)
 			
 		
-		# Remove the ball if it goes under the paddle
-		if collider.get_collider().collision_layer == 4:
-			self.free()
+		# Lose a life and reset the ball if it goes under the paddle
+		if collider.get_collider().collision_layer == 8:
+			if lives > 1:
+				lives -= 1
+				launched = 0
+				self.position.y = 430
+				self.position.x = $"/root/Main/Paddle".position.x
+			if lives == 2:
+				$"/root/Main/LivesCounter/life2".texture = load("res://sprites/miniballoutline.png")
+			if lives == 1:
+				$"/root/Main/LivesCounter/life1".texture = load("res://sprites/miniballoutline.png")
+			if lives == 0:
+				self.queue_free()
+			print("Lives: " + str(lives))
--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,10 +1,5 @@
 extends CharacterBody2D
 
-# Make the paddle move (commented code caused a lot of lag, currently trying to figure out how to fix it)
-func _process(delta):
-	if not(get_global_mouse_position().x < 120 or get_global_mouse_position().x > 520):
-		self.position.x = get_global_mouse_position().x
-	#else: if get_global_mouse_position().x < 120:
-	#	self.position.x = 120
-	#else: if get_global_mouse_position().x > 520:
-	#	self.position.x = 520
+# Make the paddle move
+func _input(event):
+	self.position.x = get_global_mouse_position().x
