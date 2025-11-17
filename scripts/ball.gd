020e8db7c0c56e0793f2332ed0bb41c4c5650a60
EshanDoes <eshan.shiraz@gmail.com>
2025-11-17 11:03:43 -0500

    Hourly Commit 4.5 (Day 8? 7?)
    Added comments to make sure it's known what code does what, made the bricks different colors, and added a bottom collision which is supposed to break the ball (but it just freezes the ball for some reason so I'll figure out a workaround later)

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,28 +3,44 @@ extends CharacterBody2D
 var speed = 200
 var initialVelocity # Initial velocity (speed and direction)
 var launched = 0
+var bricksDestroyed = 0
+var bricksGroup
 
+# Set the initial velocity because the speed variable doesn't apply until the project starts
 func _ready():
 	initialVelocity = Vector2(speed, -200)
+	bricksGroup = $"root/Main/Bricks".new()
 
+# Check if the player clicked, and launch the ball if they did, and if they didn't make the ball follow the paddle when the mouse moves
 func _input(event):
 	if event.is_action_pressed("click"):
 		launched = 1
 	if launched == 0:
 		self.position.x = $"/root/Main/Paddle".position.x - 320
 
+# Makes the ball move and bounce
 func _physics_process(delta):
-	# Move the object and detect collisions
 	var collider = move_and_collide(initialVelocity * delta * launched)
 	
 	if collider:
-		# Change direction based on collision
-		print(initialVelocity.bounce(collider.get_normal()))
-		initialVelocity = initialVelocity.bounce(collider.get_normal())
-		print(initialVelocity)
-		
+		print(collider.get_collider().name)
 		print(collider.get_collider().collision_layer)
+		
+		# Check if the ball has collided with anything that can make it bounce or a brick
+		if collider.get_collider().collision_layer == 1 or collider.get_collider().collision_layer == 2:
+			initialVelocity = initialVelocity.bounce(collider.get_normal())
+			print(initialVelocity)
+		
+		# Break a brick if the ball has collided with one
 		if collider.get_collider().collision_layer == 2:
 			collider.get_collider().queue_free()
+			bricksDestroyed += 1
+			if bricksDestroyed == 20:
+				$"rootMain".add_child(bricksGroup)
+				print("All bricks destroyed!")
+			print(bricksDestroyed)
+			
 		
-		await get_tree().create_timer(0.01).timeout
+		# Remove the ball if it goes under the paddle
+		if collider.get_collider().collision_layer == 4:
+			self.free()
