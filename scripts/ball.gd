db5d5acf840e33c851a275774c77aa58eb1c5ac2
EshanDoes <eshan.shiraz@gmail.com>
2025-11-19 14:18:48 -0500

    Hourly Commit 8.5 (Day 10)
    Removed the paddle bouncing thing until I can figure it out later, and changed the hitbox back to capsule. Also, forgot to mention last commit, but I added a life system as well.
    
    In this commit, I added a score system, organized the code a little bit, and added screen shake for when you hit a brick or lose a life. Also added a flashing effect for when you lose a life that appears on the life visual.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 var speed = 200
-var initialVelocity # Initial velocity (speed and direction)
+var initialVelocity
 var launched = 0
 var bricksDestroyed = 0
 var bricksGroup
 var lives = 3
+var score = 0
 
 # Set the initial velocity because the speed variable doesn't apply until the project starts
 func _ready():
@@ -14,7 +15,7 @@ func _ready():
 
 # Check if the player clicked, and launch the ball if they did, and if they didn't make the ball follow the paddle when the mouse moves
 func _input(event):
-	if event.is_action_pressed("click"):
+	if event.is_action_pressed("click") and !lives == 0:
 		launched = 1
 	if launched == 0:
 		self.position.x = $"/root/Main/Paddle".position.x
@@ -24,9 +25,10 @@ func _physics_process(delta):
 	var collider = move_and_collide(initialVelocity * delta * launched)
 	
 	if collider:
-		print(collider.get_collider().name)
-		print(collider.get_collider().collision_layer)
 		var collidingObject = collider.get_collider()
+		print(collidingObject.name)
+		print(collidingObject.collision_layer)
+		print(collidingObject.get_parent().name)
 		
 		# Check if the ball has collided with anything that can make it bounce or a brick
 		if collidingObject.collision_layer == 1 or  collidingObject.collision_layer == 2 or collidingObject.collision_layer == 16:
@@ -37,28 +39,59 @@ func _physics_process(delta):
 		if collidingObject.collision_layer == 2:
 			collidingObject.queue_free()
 			bricksDestroyed += 1
+			
+			# Detect how many points you get from destroying a brick
+			if collidingObject.get_parent().name == "bottom":
+				score += 3
+				screenShake(3, 10)
+			else: if collidingObject.get_parent().name == "midbottom":
+				score += 5
+				screenShake(5, 7)
+			else: if collidingObject.get_parent().name == "midtop":
+				score += 10
+				screenShake(7, 5)
+			else: if collidingObject.get_parent().name == "top":
+				score += 15
+				screenShake(9, 3)
+			$"/root/Main/Points Text".text = str(score)
+			
 			if bricksDestroyed == 20:
 				print("All bricks destroyed!")
 				# Current priority is figuring out how to fix this (Why can't I just do what I usually do in Unity aaaaaaaaaaaaaa)
 				add_sibling(bricksGroup)
 			print(bricksDestroyed)
 			
-		if collidingObject.collision_layer == 16:
-			var angleFromPaddle = (rad_to_deg(collidingObject.position.angle_to_point(self.position))+90)*2
-			print("Angle: " + str(angleFromPaddle))
-			initialVelocity = Vector2(angleFromPaddle, initialVelocity.y)
-			
 		# Lose a life and reset the ball if it goes under the paddle
 		if collidingObject.collision_layer == 8:
+			lives -= 1
+			launched = 0
 			if lives > 0:
-				lives -= 1
-				launched = 0
 				self.position.y = 430
 				self.position.x = $"/root/Main/Paddle".position.x
-			if lives == 2:
-				$"/root/Main/LivesCounter/life2".texture = load("res://sprites/miniballoutline.png")
-			if lives == 1:
-				$"/root/Main/LivesCounter/life1".texture = load("res://sprites/miniballoutline.png")
-			if lives == 0:
-				self.queue_free()
+				removeLifeTexutre()
+			screenShake(20, 250)
+			
 			print("Lives: " + str(lives))
+
+# Sparkled effect for updating the sprite when you lose a life
+func removeLifeTexutre():
+	var lifeTexture = get_node("/root/Main/LivesCounter/life" + str(lives))
+	
+	for i in 3:
+		lifeTexture.texture = load("res://sprites/miniball.png")
+		await get_tree().create_timer(0.1).timeout
+		lifeTexture.texture = load("res://sprites/miniballoutline.png")
+		await get_tree().create_timer(0.1).timeout
+
+# Really juicy screen shake effect, so much sparkle in there
+func screenShake(shakeAmount, shakeLength):
+	var camera = $"/root/Main/Camera"
+	var random = RandomNumberGenerator.new()
+	
+	for i in shakeLength:
+		camera.offset.x = round(random.randf_range(shakeAmount*-1, shakeAmount)/i*5)/5
+		camera.offset.y = round(random.randf_range(shakeAmount*-1, shakeAmount)/i*5)/5
+		
+		await get_tree().create_timer(0.01).timeout
+	
+	camera.offset = Vector2(0, 0)
